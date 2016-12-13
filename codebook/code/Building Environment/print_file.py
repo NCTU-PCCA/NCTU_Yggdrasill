ac21cd9319604cd44f78e59ff200566935f8d40a
fsh0524 <fsh0524@gmail.com>
2016-12-13 20:46:06 +0800

    rm

--- codebook/code/Building Environment/print_file.py
+++
@@ -1,7 +0,0 @@
-import sublime, sublime_plugin
-import os
-
-class print_file(sublime_plugin.TextCommand):
-	def run(self, edit):
-		os.system('cat -n "%s" > tmp.print; lpr tmp.print' % self.view.file_name())
-		self.view.show_popup("JIZZ!!")
