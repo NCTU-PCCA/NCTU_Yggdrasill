1cc0b5407dfce27e093a903c8bf3b9556d4569dc
fsh0524 <fsh0524@gmail.com>
2017-09-30 17:24:32 +0800

    ap

---
+++ codebook/code/Graph/Articulation Point.cpp
@@ -0,0 +1,19 @@
+void tarjan(int u, int p)
+{   // u 為當前點, p 為當前點之母節點
+	// cnt 為 DFS 次序
+    low[u] = dfn[u] = ++cnt;
+    int i, v;
+    for (i = 0 ; i < G[u].size() ; ++i) {
+        v = G[u][i];
+        if (u == rt && !dfn[v]) ++c;
+        if (!dfn[v]){
+        	// (u, v) 為 Tree Edge
+            tarjan(v, u);
+            low[u] = min(low[u], low[v]);
+            // To check if u is AP or not.
+            if (dfn[u] <= low[v] && u != rt) ge[u] = 1;
+        }
+        // 注意不可以同一條邊走兩次，且根節點特判
+        if (v != p && p != -1) low[u] = min(low[u], dfn[v]);
+    }
+}
